4c84b1c37fc382e0db853c787d334835c213b2f3
RamFernando <rameshafernando1@gmail.com>
2020-02-09 11:42:14 +0530

    second commit

--- springbootpractise1/src/main/java/com/ramesha/training/model/Employee.java
+++ springbootpractise1/src/main/java/com/ramesha/training/model/Employee.java
@@ -4,43 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Employee {
+
 	String employeeName;
-	int empMarks;
-	
-	public Employee() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Employee(String employeeName, int empMarks) {
-		this.employeeName=employeeName;
-		this.empMarks=empMarks;
-	}
-	
+	String employeeLocation;
 	public String getEmployeeName() {
 		return employeeName;
 	}
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
-	public int getEmpMarks() {
-		return empMarks;
+	public String getEmployeeLocation() {
+		return employeeLocation;
 	}
-	public void setEmpMarks(int empMarks) {
-		this.empMarks = empMarks;
+	public void setEmployeeLocation(String employeeLocation) {
+		this.employeeLocation = employeeLocation;
 	}
 	
 	public static List<Employee> getAllEmployees(){
-		List<Employee> employees = new ArrayList<>();
+		List<Employee> employees = new ArrayList<Employee>();
 		
 		Employee employee = new Employee();
 		employee.setEmployeeName("Ramesha");
-		employee.setEmpMarks(92);
+		employee.setEmployeeLocation("Kottawa");
 		employees.add(employee);
-		
-		Employee employee1 = new Employee();
-		employee1.setEmployeeName("Kavisha");
-		employee1.setEmpMarks(98);
-		employees.add(employee1);
 		
 		return employees;
 	}
