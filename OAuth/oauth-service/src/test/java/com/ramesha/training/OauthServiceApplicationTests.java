e787c60a9986e44a3819c03e0fc113781a20c6af
ramesha <rameshafernando1@gmail.com>
2020-03-01 19:03:34 +0530

    OAuth

---
+++ OAuth/oauth-service/src/test/java/com/ramesha/training/OauthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesha.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
