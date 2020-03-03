a09a274732822147e644113d157fee0b5ae7db96
RamFernando <rameshafernando1@gmail.com>
2020-03-03 11:43:54 +0530

    Local Congifuration

---
+++ Practise Examples/config-local/src/main/java/com/ramesha/rentcloud/configlocal/RentcloudLocalApplication.java
@@ -0,0 +1,15 @@
+package com.ramesha.rentcloud.configlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class RentcloudLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentcloudLocalApplication.class, args);
+	}
+
+}
---
+++ Practise Examples/config-local/src/test/java/com/ramesha/rentcloud/configlocal/RentcloudLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesha.rentcloud.configlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentcloudLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
